571bb2e7db0da0f11aa638900ea97417dd8cab0b
Ruslan Volovenko <vokoloven@gmail.com>
2022-11-13 13:32:32 +0200

    update

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,17 +1,23 @@
 import { Box } from 'Theme/Box';
 // import { StyledLink } from './StyledLink';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
+import { ColorButton } from './ColorButton';
+// import { backgroundColor } from 'styled-system';
 
 export const AuthNav = () => {
   return (
-    <Box ml="auto" mr={3}>
-      <Button color="inherit">
-        <NavLink to="registration">Registration</NavLink>
-      </Button>
-      <Button color="inherit">
-        <NavLink to="login">Log In</NavLink>
-      </Button>
+    <Box ml="auto" mr={3} display="flex">
+      <Box mr={3}>
+        <ColorButton>
+          <NavLink to="registration">Registration</NavLink>
+        </ColorButton>
+      </Box>
+      <Box>
+        <ColorButton>
+          <NavLink to="login">Log In</NavLink>
+        </ColorButton>
+      </Box>
     </Box>
   );
 };
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,7 +2,8 @@ import { Box } from 'Theme/Box';
 import { useSelector, useDispatch } from 'react-redux';
 import { userData } from 'redux/selectors';
 import operations from 'redux/auth/auth-operations';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
+import { ColorButton } from 'components/AuthNav/ColorButton';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -28,9 +29,9 @@ export const UserMenu = () => {
       >
         Добро пожаловать {name}
       </Box>
-      <Button variant="contained" type="button" onClick={logOutUser}>
+      <ColorButton type="button" onClick={logOutUser}>
         Log Out
-      </Button>
+      </ColorButton>
       {/* <StyledButton type="button" onClick={logOutUser}>
         Log Out
       </StyledButton> */}
