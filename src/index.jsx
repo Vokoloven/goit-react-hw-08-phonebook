4153e3ae231e0e5ca11c47e4b4b06035c0346e0f
Ruslan Volovenko <vokoloven@gmail.com>
2022-11-13 12:49:27 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-08-phonebook">
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
